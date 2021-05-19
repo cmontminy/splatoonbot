677f94171b95d02d36d32947d02f08d79c8f3915
Colette Montminy <cmontminy@utexas.edu>
2021-05-19 17:07:15 -0500

    stupid python

--- splat.py
+++ splat.py
@@ -9,18 +9,18 @@ import traceback
 import asyncio
 import json
 import psycopg2
-from discord.ext   import commands
+from discord.ext import commands
 from discord_slash import SlashCommand
 from discord_slash.utils.manage_commands import create_option, create_choice
 
-from commands.maps  import Maps
+from commands.maps import Maps
 from commands.admin import Admin
 
 
-DATABASE_URL = os.environ['DATABASE_URL'] # connect to postgres if online
+DATABASE_URL = os.environ['DATABASE_URL']  # connect to postgres if online
 connection = psycopg2.connect(DATABASE_URL, sslmode='require')
 # connection = sqlite3.connect("splat.db")
-cursor     = connection.cursor()
+cursor = connection.cursor()
 
 maps = {}
 
@@ -30,46 +30,52 @@ command_prefix = '!'
 description = "Discord bot to help with Splatoon things"
 bot = commands.Bot(command_prefix=command_prefix, description=description,
                    case_insensitive=True)
-slash = SlashCommand(bot, sync_commands=True) # Declares slash commands through the client.
+# Declares slash commands through the client.
+slash = SlashCommand(bot, sync_commands=True)
 
 # COGS
 bot.add_cog(Maps(bot))
 bot.add_cog(Admin(bot))
 
 # start up
+
+
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user.name}")
 
     game = discord.Game("stinky is cute !")
-    await bot.change_presence(activity = game)
+    await bot.change_presence(activity=game)
 
     with open("maps") as infile:
         for line in infile:
             data = line.split("=")
             maps[data[0]] = data[1]
 
-guild_ids = [670469511572488223, 771226056346042410] # bot testing id, splat server
+# bot testing id, splat server
+guild_ids = [670469511572488223, 771226056346042410]
+
 
 @slash.slash(name="ping", guild_ids=guild_ids)
-async def _ping(ctx): # Defines a new "context" (ctx) command called "ping."
+async def _ping(ctx):  # Defines a new "context" (ctx) command called "ping."
     await ctx.send(f"Pong! ({bot.latency*1000}ms)")
 
-@slash.slash(name="callouts", 
+
+@slash.slash(name="callouts",
              description="Pulls pictures of callouts or rainmaker paths",
              options=[
-               create_option(
-                 name="rm",
-                 description="True = displays rm path for the map",
-                 option_type=5,
-                 required=False
-               ),
-               create_option(
-                 name="map",
-                 description="Name or short code of the map",
-                 option_type=3,
-                 required=True
-               )
+                 create_option(
+                     name="map",
+                     description="Name or short code of the map",
+                     option_type=3,
+                     required=True
+                 ),
+                 create_option(
+                     name="rm",
+                     description="True = displays rm path for the map",
+                     option_type=5,
+                     required=False
+                 )
              ],
              guild_ids=guild_ids)
 async def _callouts(ctx, rm: bool, map: str):
@@ -95,4 +101,4 @@ bot.run(os.environ.get('TOKEN'))
 #     for line in fl:
 #         if "TOKEN" in line:
 #             bot_token = line.split("=")[1]
-# bot.run(bot_token)   
+# bot.run(bot_token)
